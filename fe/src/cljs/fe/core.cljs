f29f0d7efab5442811f58a00ed783eb8d4051027
Bahadir Cambel <bcambel@gmail.com>
2014-11-11 12:54:51 +0100

    registry def

--- fe/src/cljs/fe/core.cljs
+++ fe/src/cljs/fe/core.cljs
@@ -15,7 +15,22 @@
       {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
       {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
       {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]
-  }))
+      :people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker" 
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman" 
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu" 
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"
+      :1806 "Linear Algebra"}
+  }
+  ))
 
 (defn parse-contact [contact-str]
   (let [[first middle last :as parts] (string/split contact-str #"\s+")
@@ -44,6 +59,32 @@
 
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
+
+
+
+(defn student-view [student owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li nil
+        (dom/div nil (display-name professor))
+        (dom/label nil "Classes")
+        (apply dom/ul nil
+          (map #(dom/li nil %) (:classes professor)))))))
+
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner] (professor-view person owner))
 
 (defn contact-view [contact owner]
   (reify
@@ -88,8 +129,26 @@
                           :onChange #(handle-change % owner state)})
           (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
 
+(defn people [app]
+  (->> (:people app)
+    (mapv (fn [x]
+            (if (:classes x)
+              (update-in x [:classes]
+                (fn [cs] (mapv (:classes app) cs)))
+               x)))))
+
+(defn registry-view [app owner]
+  (reify
+    om/IRenderState
+    (render-state [_ state]
+      (dom/div #js {:id "registry"}
+        (dom/h2 nil "Registry")
+        (apply dom/ul nil
+          (om/build-all entry-view (people app)))))))
+
+
 (defn main []
 (om/root
-  contacts-view
+  registry-view
   app-state
   {:target (. js/document (getElementById "app"))}))
