869f28dd6c0746da3a3b32a68afe711640b95624
Bahadir Cambel <bcambel@gmail.com>
2014-12-05 20:58:41 +0100

    add post controller

---
+++ src/clj/hsm/controllers/post.clj
@@ -0,0 +1,26 @@
+(ns hsm.controllers.post
+  (:require [clojure.tools.logging :as log]
+            [clojure.java.io :as io]
+            [cheshire.core :refer :all]
+            [ring.util.response :as resp]
+            [hsm.actions :as actions]
+            [hsm.utils :as utils :refer [json-resp]]))
+
+(defn create-link
+  [db request] 
+  (log/warn request)
+  (let [host  (get-in request [:headers "host"])
+    body (parse-string (utils/body-as-string request))
+    user 243975551163827208
+    link-data (utils/mapkeyw body)]
+    (actions/create-link db link-data user)
+    (json-resp { :ok body })))
+
+(defn upvote-link
+  [db request]
+  (let [host  (get-in request [:headers "host"])
+    body (parse-string (utils/body-as-string request))
+    link-id (get-in request [:route-params :id])
+    user 243975551163827208]
+    (actions/upvote-post db )
+    ))
