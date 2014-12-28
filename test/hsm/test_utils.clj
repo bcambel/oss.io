50bde4295f145246f6754fa3e9cd44e3bd224491
Bahadir Cambel <bcambel@gmail.com>
2014-12-28 15:27:08 +0100

    more tests for utils

--- test/hsm/test_utils.clj
+++ test/hsm/test_utils.clj
@@ -2,7 +2,7 @@
 	(:use midje.sweet)
 	(:require		[hsm.utils :refer :all]))
 
-(tabular 
+(tabular
 	(fact "Domain name resolution"
 		(domain-of {:headers { "host" ?host }} ) => ?result )
 		?host ?result
@@ -10,14 +10,14 @@
 		"pythonhackers.com" 				["pythonhackers" "com"]
 		"www.clojurehackers.com" 		["www" "clojurehackers" "com"])
 
-(tabular 
+(tabular
 	(fact "Find Host of the Request"
 		(host-of {:headers { "host" ?host }} ) => ?result)
 		?host ?result
 		 "dev.pythonhackers.com" "dev.pythonhackers.com"
 		 "clojurehackers.com" "clojurehackers.com")
 
-(tabular 
+(tabular
 	(fact "Empty Host Request with falsy hash-map "
 		(host-of ?request ) => ?result)
 		?request ?result
@@ -25,9 +25,31 @@
 		 {:headers2 "test"} nil)
 
 ; Well Token fetcher always returns the same ID for right now! Very effective!! :)
-(tabular 
+(tabular
 	(fact "Find User Token"
 		(whois {:headers { "X-AUTH-TOKEN" ?token }}) => ?result)
 		?token ?result
 		1 243975551163827208
 		)
+
+(tabular
+	(fact "Select Values of a Hash-Map"
+		(select-values ?hashmap ?keys) => ?result)
+		?hashmap ?keys ?result
+		{:a 1 :b 2} [:a :b] [1 2]
+		{:a 1 :b 2} [:a] [1]
+		{:a 1 :b 2} [] []
+		{:a 1 :b 2} nil []
+		)
+
+(let [temp-file (java.io.File/createTempFile (str "body-test" (now->ep)) ".txt")]
+	(doto (clojure.java.io/writer temp-file)
+		(.write "testing")
+		(.close))
+	(tabular
+		(fact "Body Slurping"
+			(body-as-string {:body ?body}) => ?result)
+			?body ?result
+			"test" "test"
+			temp-file "testing")
+	(.delete temp-file))
