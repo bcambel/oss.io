2f0a621280cec97392f615b7ad773f9802c2b6ca
Bahadir Cambel <bcambel@gmail.com>
2015-01-08 21:09:42 +0100

    add tracking code

--- src/clj/hsm/views.clj
+++ src/clj/hsm/views.clj
@@ -4,6 +4,7 @@
     [hiccup.core        :refer [html]]
     [hiccup.page        :refer [doctype include-css include-js]]
     [hiccup.def         :refer [defhtml]]
+    [hsm.dev            :refer [is-dev?]]
     [hsm.conf           :refer [languages]]))
 
 (defhtml row-fluid
@@ -36,15 +37,22 @@
     [:tr [:td
     [:a {:href (format "/%s/top-projects" lang)} lang]]])])
 
+(def propert-ids 
+  { "hackersome.com" "UA-57973731-1"
+    "sweet.io" "UA-33058338-1"
+    "pythonhackers.com" "UA-42128994-1"
+    "clojurehackers.com" "UA-57973731-3"
+    "dev.hackersome.com" "UA-57973731-1" })
+
 (defhtml layout
   [website & content]
+  (let [property-id (get propert-ids website)]
   (:html5 doctype)
   [:html {:lang "en-US"}
    [:head
     [:meta {:charset "UTF-8"}]
     [:meta {:content "IE=edge,chrome=1"
             :http-equiv "X-UA-Compatible"}]
-    [:meta {:name "google-site-verification" :content "TbfTN22-TdrZU60a5X6UGJCHQXMYJN2dvVb5Ibn0C6E"}]
     [:title "Hackersome"]
     (include-css "//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/flatly/bootstrap.min.css")
     (include-css "//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css")  
@@ -72,5 +80,14 @@
       [:div.col-lg-1]
      ]
     (include-js "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
-    (include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")   
-    ]])
+    (include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (when (if (nil? is-dev?) true is-dev?)
+      [:script {:type "text/javascript"}
+      (format "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', '%s', 'auto');
+  ga('send', 'pageview');" property-id)]
+      )
+    ]]))
