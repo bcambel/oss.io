6a42eb8b0bbd42200223f86f38282f9af403480f
Bahadir Cambel <bcambel@gmail.com>
2015-01-10 07:39:04 +0100

    add collection basics

--- src/clj/hsm/controllers/coll.clj
+++ src/clj/hsm/controllers/coll.clj
@@ -5,7 +5,18 @@
     [hsm.ring :refer [json-resp html-resp]]
     [hsm.utils :refer [type-of id-of host-of]]))
 
+(defn add-p-coll
+  [{:keys [db event-chan redis conf]} request])
 
+(defn del-p-coll
+  [{:keys [db event-chan redis conf]} request])
+
+(defn get-coll
+  [{:keys [db event-chan redis conf]} request])
+
+(defn create-coll
+  [{:keys [db event-chan redis conf]} request] 
+  )
 (defn load-coll
   [[db event-chan] request]
   (let [host (host-of request)
@@ -16,12 +27,13 @@
       (layout host
         [:div
           (for [c colls]
+            [:div.col-lg-3
             (panel 
               [:h3 (:name c)]
               [:div 
                 [:p (:description c)]
                 (for [item (keys (:items c))]
                   (let [el (get item (:items c))]
-                    [:li (str item el)]))]))]))))
+                    [:li [:a {:href (str "/p/" (str item el))} (str item el)]]))])])]))))
 
 
