0ce7a074a1acc303d4a2a142af57de00cac9f31f
Bahadir Cambel <bcambel@gmail.com>
2015-01-10 08:52:06 +0100

    add redirect after add/delete ops

--- src/clj/hsm/controllers/coll.clj
+++ src/clj/hsm/controllers/coll.clj
@@ -2,9 +2,10 @@
   (:require 
     [cheshire.core :refer :all]
     [clojure.tools.logging :as log]
+    [hiccup.def                   :refer [defhtml]]
     [hsm.actions :as actions]
     [hsm.views :refer [layout panel]]
-    [hsm.ring :refer [json-resp html-resp]]
+    [hsm.ring :refer [json-resp html-resp redirect]]
     [hsm.utils :refer [type-of id-of host-of body-of]]))
 
 (defn add-p-coll
@@ -22,8 +23,7 @@
       (log/debug "Before" items)
       (log/debug "After" new-items)
       (actions/update-collection db id new-items)
-      (json-resp coll)
-  )))
+      (redirect (format "/collections/%s%s" id (if is-json "?json=1" ""))))))
 
 (defn del-p-coll
   [{:keys [db event-chan redis conf]} request]
@@ -40,17 +40,38 @@
       (log/debug "Before" items)
       (log/debug "After" new-items)
       (actions/update-collection db id new-items)
-      (json-resp coll)
-  )))
+      (redirect (format "/collections/%s%s" id (if is-json "?json=1" ""))))))
+
+(defhtml render-collection
+  [c]
+  [:div.col-lg-3
+    (panel
+      [:h3 [:a {:href (str "/collections/" (:id c))}(:name c)]]
+      [:div 
+        [:p (:description c)]
+        (for [item (keys (:items c))]
+          (let [el (get item (:items c))]
+            [:li [:a {:href (str "/p/" (str item el))} (str item el)]
+              [:form {:method "POST" :action (format "/collections/%s/delete" (:id c))}
+                [:input {:type "hidden" :name :project :value item}]
+                [:button.btn.btn-danger.btn-xs {:type :submit} "X"]
+              ]  
+            ]))
+        [:form {:method "POST" :action (format "/collections/%s/add" (:id c))}
+          [:input {:type "text" :name :project}]
+          [:button.btn.btn-primary {:type :submit} "Add to"]
+        ]])])
 
 (defn get-coll
   [{:keys [db event-chan redis conf]} request]
   (let [host (host-of request)
-        is-json (type-of request :json)
+        is-json? (type-of request :json)
         id (Integer/parseInt (id-of request))
-        coll (actions/get-collection db id)]
+        coll (first (actions/get-collection db id))]
+    (if is-json?
       (json-resp coll)
-  ))
+      (layout host
+        (render-collection coll)))))
   
 
 (defn create-coll
@@ -66,24 +87,7 @@
       (layout host
         [:div
           (for [c colls]
-            [:div.col-lg-3
-            (panel 
-              [:h3 [:a {:href (str "/collections/" (:id c))}(:name c)]]
-              [:div 
-                [:p (:description c)]
-                (for [item (keys (:items c))]
-                  (let [el (get item (:items c))]
-                    [:li [:a {:href (str "/p/" (str item el))} (str item el)]
-                      [:form {:method "POST" :action (format "/collections/%s/delete" (:id c))}
-                        [:input {:type "hidden" :name :project :value item}]
-                        [:button.btn.btn-danger.btn-xs {:type :submit} "X"]
-                      ]  
-                    ]))
-                [:form {:method "POST" :action (format "/collections/%s/add" (:id c))}
-                  [:input {:type "text" :name :project}]
-                  [:button.btn.btn-primary {:type :submit} "Add to"]
-                ]
-
-                ])])]))))
+            (render-collection c)
+            )]))))
 
 
