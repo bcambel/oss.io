b262a553622e33c7c3e1774a5e3e2f72444829e5
Bahadir Cambel <bcambel@gmail.com>
2015-01-31 18:08:40 +0100

    more cassandra system tasks

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric.api import run, sudo, env, task, local, put, parallel
 from fabric.context_managers import cd
+from fabric.contrib.console import confirm
 import logging
 from fabtools import require
 
@@ -27,6 +28,12 @@ def cassax(command='status'):
 def runx(command):
     run("{}".format(command))
 
+
+@task
+def aptupdate():
+  sudo("apt-get update --fix-missing")
+
+
 @task
 def set_up():
   sudo("adduser cassa --system")
@@ -34,10 +41,48 @@ def set_up():
   require.deb.packages(PACKAGES)
 
 @task
+def generate_cassandra_settings(ip_address):
+  text = open("conf/cassandra.yaml").read()
+  text = text.replace("<ip_address>",ip_address)
+
+  with open("conf/.cassandra.yaml.temp", "w") as f:
+    f.write(text)
+
+  put("conf/.cassandra.yaml.temp", "/etc/cassandra/cassandra.yaml")
+
+@task
+def cassandra(ip_address):
+  if not confirm("Are you sure to install followings ? \n{}".format("=>\n".join([e for e in env.hosts]))):
+    return 
+
+  sudo("curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -")
+  sudo('echo "deb http://debian.datastax.com/community stable main" > /etc/apt/sources.list.d/datastax.list')
+  aptupdate()
+  
+  sudo("apt-get install -y cassandra=2.0.10 dsc20=2.0.10-1")
+
+  sudo("service cassandra stop")
+  sudo("rm -rf /var/lib/cassandra/data/system/*")
+
+  put("conf/log4j-server.properties", "/etc/cassandra/log4j-server.properties")
+  sudo("mv /etc/cassandra/cassandra.yaml /etc/cassandra/cassandra2.yaml")
+
+  generate_cassandra_settings(ip_address)
+
+  
+
+@task
+def cassandra_agent(datastax_master):
+  require.deb.package(["datastax-agent"])
+  sudo('echo "stomp_interface: {}" | sudo tee -a /var/lib/datastax-agent/conf/address.yaml'.format(datastax_master))
+  sudo("service datastax-agent start")
+
+
+@task
 def rpm(license_key):
   sudo("echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list")
   sudo("wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -")
-  sudo("apt-get update --fix-missing")
+  aptupdate()
   sudo("apt-get install newrelic-sysmond")
   sudo("nrsysmond-config --set license_key={}".format(license_key))
   sudo("/etc/init.d/newrelic-sysmond start")
