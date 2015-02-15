10a288a05e6dff16e97c677ae5e14e5d139c15c2
Bahadir Cambel <bcambel@gmail.com>
2015-02-15 12:51:41 +0100

    Ego boost by updating
    Newsletter subscribers count manually

--- src/clj/hsm/views.clj
+++ src/clj/hsm/views.clj
@@ -41,7 +41,7 @@
   [:div.bs-callout.bs-callout-success ;{:style "background-color:#f7f7f7;" }
   
     [:a.btn.btn-success {:href "#mc_embed_signup" :data-toggle :modal} "Subscribe Free"]
-    [:p {:style "margin-top:10px"} "Join " [:b 944] " others. No spamming." [:br][:b "I promise!"]]
+    [:p {:style "margin-top:10px"} "Join " [:b 953] " others. No spamming." [:br][:b "I promise!"]]
     [:p "We are currently under high development. "[:a {:href "https://github.com/bcambel/hackersome?utm_source=left_menu_link"} "Follow us at github."]]
     [:hr]
     [:p "Looking for " [:b[:span.red "Python Tutorials? "]] [:br] [:a {:href "/tutorial/?utm_source=left_menu_link"}  "Check these awesome tutorials"]]
